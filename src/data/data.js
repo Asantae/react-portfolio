fcebebfb933e32d3c35b76cb6842adee412ecacb
Asantae Shelton <asantaems@gmail.com>
2022-11-23 01:15:10 -0500

    initializing repo

---
+++ src/data/data.js
@@ -0,0 +1,20 @@
+import WorkIm from '../assets/projects/workImg.jpeg'
+import RealEst from '../assets/projects/realestate.jpg'
+
+export const data=[
+    {
+        id:1,
+        name:"Socialite",
+        image:WorkIm,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"Stargazer Society",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+
+]
