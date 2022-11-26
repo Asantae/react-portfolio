b6e373cbb3b3138657729799b6bc52c12137fb6d
Asantae Shelton <asantaems@gmail.com>
2022-11-26 18:39:43 -0500

    minor changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
-        <img src={Logo} alt='Logo' style={{ width: '75px' }} />
+        <img src={Logo} alt='Logo' style={{ width: '65px' }} />
       </div>
 
       {/* menu */}
